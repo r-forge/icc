f2098dca9dd8aa42d9ce87ac158405a7cd327c4f
mwola (Matthew Wolak) <matthewwolak@gmail.com>
2011-03-08 01:23:59 +0000

    cleaned up ICCest R file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/icc@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ICCest.r
+++ pkg/R/ICCest.r
@@ -1,6 +1,3 @@
-#Below takes a dataframe of a trait and individuals with multiple measurements on each individual and calculates the repeatability.
-# Alpha is the size of the confidence interval (95% default).  x is the groups or individuals and y is the trait (measurements).
-
 ICCest<-function(x, y, data=data, alpha=0.05){
   if(is.data.frame(data)==FALSE)
        stop(paste("object dataframe is of the type", class(data), "and must be of type data.frame", sep=" "))
@@ -9,7 +6,7 @@ ICCest<-function(x, y, data=data, alpha=0.05){
   xc<-as.character(ICCcall[[2L]])
   yc<-as.character(ICCcall[[3L]])
   inds<-unique(data[xc])[[1]]
-  a<-length(inds)#"a" from Lessells&Boag(# groups/individuals)
+  a<-length(inds)
   tdata<-data.frame(data[yc], data[xc])
 
   tmpbb<-anova.lm(aov(tdata[,1]~tdata[,2], data=tdata))
@@ -17,18 +14,18 @@ ICCest<-function(x, y, data=data, alpha=0.05){
   denom.df<-tmpbb[1][2,1];
   MSa<-tmpbb[3][1,1]
   MSw<-tmpbb[3][2,1]
+  
   tmp.outj<-data.frame(lapply(unstack(na.omit(tdata)), FUN=length))
   k<-(1/(a-1))*(sum(tmp.outj)-(sum(square(tmp.outj))/(sum(tmp.outj))))
-      #tmp.outj is N subscript i from Lessells & Boag
   
   var.w<-MSw
   var.a<-(MSa-MSw)/(k)
-  r<-var.a/(var.w + var.a) #r="r" from Lessells & Boag
-
-#Below computes the 95% confidence interval of the repeatability, from Becker 1984 which references Bogyo and Becker 1963.  The normality of the repeatability doesn't have to be assumed, as with computing the standard error of the repeatability.
+  r<-var.a/(var.w + var.a)
+  
   low.F<-qf(alpha/2, num.df, denom.df, lower.tail=FALSE)
   up.F<-1/(qf(1-alpha/2, denom.df, num.df, lower.tail=TRUE))
   low.CI<-1-((k*MSw*low.F)/(MSa+MSw*(k-1)*low.F))
   up.CI<-1-((k*MSw*up.F)/(MSa+MSw*(k-1)*up.F))
+
 list(ICC=r, LowerCI=low.CI, UpperCI=up.CI, N=a, k=k, varw=var.w, vara=var.a)
 }
